9d72598441a2e7e38eeacd2e7cb6f17282bcdf81
Jérôme Petazzoni <jp@enix.org>
2012-03-08 17:55:43 -0800

    move dependencies from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     author       = 'dotCloud inc. <team@dotcloud.com>',
     package_dir  = {'zerorpc': '.'},
     packages     = ['zerorpc'],
+    install_requires = [
+            'argparse',
+            'gevent',
+            'msgpack-python',
+            'pyzmq-static==2.1.7',
+    ],
     zip_safe     = False,
     scripts      = [
             'bin/zerorpc-client'
