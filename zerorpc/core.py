04ed696312b9da55719f364046f911aa70f6db18
François-Xavier Bourlet <fx@dotcloud.com>
2012-03-25 02:48:30 -0700

    Client/Server split in {Client/Server}Base

--- zerorpc/core.py
+++ zerorpc/core.py
@@ -39,6 +39,204 @@ from .socket import SocketBase
 from .heartbeat import HeartBeatOnChannel
 
 
+class ServerBase(object):
+
+    def __init__(self, channel, methods=None, name=None, pool_size=None,
+            heartbeat=5):
+        self._multiplexer = ChannelMultiplexer(channel)
+
+        if methods is None:
+            methods = self
+
+        self._name = name or repr(methods)
+        self._task_pool = gevent.pool.Pool(size=pool_size)
+        self._acceptor_task = None
+
+        if hasattr(methods, '__getitem__'):
+            self._methods = methods
+        else:
+            server_methods = set(getattr(self, k) for k in dir(Server) if not
+                    k.startswith('_'))
+            self._methods = dict((k, getattr(methods, k))
+                    for k in dir(methods)
+                    if callable(getattr(methods, k))
+                    and not k.startswith('_')
+                    and getattr(methods, k) not in server_methods
+                    )
+
+        self._inject_builtins()
+        self._heartbeat_freq = heartbeat
+
+        for (k, functor) in self._methods.items():
+            if not isinstance(functor, DecoratorBase):
+                self._methods[k] = rep(functor)
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        self.stop()
+        self._multiplexer.close()
+
+    def _zerorpc_inspect(self, method=None, long_doc=True):
+        if method:
+            methods = {method: self._methods[method]}
+        else:
+            methods = dict((m, f) for m, f in self._methods.items()
+                    if not m.startswith('_'))
+        detailled_methods = [(m, f._zerorpc_args(),
+            f.__doc__ if long_doc else
+            f.__doc__.split('\n', 1)[0] if f.__doc__ else None)
+                for (m, f) in methods.items()]
+        return {'name': self._name,
+                'methods': detailled_methods}
+
+    def _inject_builtins(self):
+        self._methods['_zerorpc_list'] = lambda: [m for m in self._methods
+                if not m.startswith('_')]
+        self._methods['_zerorpc_name'] = lambda: self._name
+        self._methods['_zerorpc_ping'] = lambda: ['pong', self._name]
+        self._methods['_zerorpc_help'] = lambda m: self._methods[m].__doc__
+        self._methods['_zerorpc_args'] = \
+            lambda m: self._methods[m]._zerorpc_args()
+        self._methods['_zerorpc_inspect'] = self._zerorpc_inspect
+
+    def __call__(self, method, *args):
+        if method not in self._methods:
+            raise NameError(method)
+        return self._methods[method](*args)
+
+    def _async_task(self, initial_event):
+        protocol_v2 = initial_event.header.get('v', 1) >= 2
+        channel = self._multiplexer.channel(initial_event)
+        hbchan = HeartBeatOnChannel(channel, freq=self._heartbeat_freq,
+                passive=not protocol_v2)
+        bufchan = BufferedChannel(hbchan)
+        event = bufchan.recv()
+        try:
+            functor = self._methods.get(event.name, None)
+            if functor is None:
+                raise NameError(event.name)
+            functor.pattern.process_call(self._context, bufchan, event, functor)
+        except Exception:
+            try:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_exception(exc_type, exc_value, exc_traceback,
+                        file=sys.stderr)
+                human_traceback = traceback.format_exc()
+
+                name = exc_type.__name__
+                human_msg = str(exc_value)
+
+                if protocol_v2:
+                    bufchan.emit('ERR', (name, human_msg, human_traceback))
+                else:
+                    bufchan.emit('ERR', (repr(exc_value),))
+            finally:
+                del exc_traceback
+        finally:
+            bufchan.close()
+            bufchan.channel.close()
+            bufchan.channel.channel.close()
+
+    def _acceptor(self):
+        while True:
+            initial_event = self._multiplexer.recv()
+            self._task_pool.spawn(self._async_task, initial_event)
+
+    def run(self):
+        self._acceptor_task = gevent.spawn(self._acceptor)
+        try:
+            self._acceptor_task.get()
+        finally:
+            self._acceptor_task = None
+            self._task_pool.join(raise_error=True)
+
+    def stop(self):
+        if self._acceptor_task is not None:
+            self._acceptor_task.kill(block=False)
+
+
+class ClientBase(object):
+
+    def __init__(self, channel, patterns, timeout=30, heartbeat=5,
+            passive_heartbeat=False):
+        self._multiplexer = ChannelMultiplexer(channel,
+                ignore_broadcast=True)
+        self._patterns = patterns
+        self._timeout = timeout
+        self._heartbeat_freq = heartbeat
+        self._passive_heartbeat = passive_heartbeat
+
+    def __del__(self):
+        self.close()
+        super(Client, self).__del__()
+
+    def close(self):
+        self._multiplexer.close()
+        super(Client, self).close()
+
+    def _raise_remote_error(self, event):
+        self._context.middleware_raise_error(event)
+
+        if event.header.get('v', 1) >= 2:
+            (name, msg, traceback) = event.args
+            raise RemoteError(name, msg, traceback)
+        else:
+            (msg,) = event.args
+            raise RemoteError('RemoteError', msg, None)
+
+    def _select_pattern(self, event):
+        for pattern in self._patterns:
+            if pattern.accept_answer(event):
+                return pattern
+        msg = 'Unable to find a pattern for: {0}'.format(event)
+        raise RuntimeError(msg)
+
+    def _process_response(self, method, bufchan, timeout):
+        try:
+            try:
+                event = bufchan.recv(timeout)
+            except TimeoutExpired:
+                raise TimeoutExpired(timeout,
+                        'calling remote method {0}'.format(method))
+
+            pattern = self._select_pattern(event)
+            return pattern.process_answer(self._context, bufchan, event, method,
+                timeout, self._raise_remote_error)
+        except:
+            bufchan.close()
+            bufchan.channel.close()
+            bufchan.channel.channel.close()
+            raise
+
+    def __call__(self, method, *args, **kargs):
+        timeout = kargs.get('timeout', self._timeout)
+        channel = self._multiplexer.channel()
+        hbchan = HeartBeatOnChannel(channel, freq=self._heartbeat_freq,
+                passive=self._passive_heartbeat)
+        bufchan = BufferedChannel(hbchan, inqueue_size=kargs.get('slots', 100))
+
+        bufchan.emit(method, args)
+
+        try:
+            if kargs.get('async', False) is False:
+                return self._process_response(method, bufchan, timeout)
+
+            async_result = gevent.event.AsyncResult()
+            gevent.spawn(self._process_response, method, bufchan,
+                    timeout).link(async_result)
+            return async_result
+        except:
+            bufchan.close()
+            bufchan.channel.close()
+            bufchan.channel.channel.close()
+            raise
+
+    def __getattr__(self, method):
+        return lambda *args, **kargs: self(method, *args, **kargs)
+
+
 class DecoratorBase(object):
     pattern = None
 
@@ -121,205 +319,30 @@ class stream(DecoratorBase):
     pattern = PatternReqStream()
 
 
-class Server(SocketBase):
+class Server(SocketBase, ServerBase):
+
     def __init__(self, methods=None, name=None, context=None, pool_size=None,
             heartbeat=5):
-        super(Server, self).__init__(zmq.XREP, context)
-        self._multiplexer = ChannelMultiplexer(self._events)
-
-        if methods is None:
-            methods = self
-
-        self._name = name or repr(methods)
-        self._task_pool = gevent.pool.Pool(size=pool_size)
-        self._acceptor_task = None
-
-        if hasattr(methods, '__getitem__'):
-            self._methods = methods
-        else:
-            server_methods = set(getattr(self, k) for k in dir(Server) if not
-                    k.startswith('_'))
-            self._methods = dict((k, getattr(methods, k))
-                    for k in dir(methods)
-                    if callable(getattr(methods, k))
-                    and not k.startswith('_')
-                    and getattr(methods, k) not in server_methods
-                    )
-
-        self._inject_builtins()
-        self._heartbeat_freq = heartbeat
-
-        for (k, functor) in self._methods.items():
-            if not isinstance(functor, DecoratorBase):
-                self._methods[k] = rep(functor)
-
-    def __del__(self):
-        self.close()
-        super(Server, self).__del__()
+        SocketBase.__init__(self, zmq.XREP, context)
+        ServerBase.__init__(self, self._events, methods, name, pool_size, heartbeat)
 
     def close(self):
-        self.stop()
-        self._multiplexer.close()
-        super(Server, self).close()
-
-    def _zerorpc_inspect(self, method=None, long_doc=True):
-        if method:
-            methods = {method: self._methods[method]}
-        else:
-            methods = dict((m, f) for m, f in self._methods.items()
-                    if not m.startswith('_'))
-        detailled_methods = [(m, f._zerorpc_args(),
-            f.__doc__ if long_doc else
-            f.__doc__.split('\n', 1)[0] if f.__doc__ else None)
-                for (m, f) in methods.items()]
-        return {'name': self._name,
-                'methods': detailled_methods}
-
-    def _inject_builtins(self):
-        self._methods['_zerorpc_list'] = lambda: [m for m in self._methods
-                if not m.startswith('_')]
-        self._methods['_zerorpc_name'] = lambda: self._name
-        self._methods['_zerorpc_ping'] = lambda: ['pong', self._name]
-        self._methods['_zerorpc_help'] = lambda m: self._methods[m].__doc__
-        self._methods['_zerorpc_args'] = \
-            lambda m: self._methods[m]._zerorpc_args()
-        self._methods['_zerorpc_inspect'] = self._zerorpc_inspect
-
-    def __call__(self, method, *args):
-        if method not in self._methods:
-            raise NameError(method)
-        return self._methods[method](*args)
-
-    def _async_task(self, initial_event):
-        protocol_v2 = initial_event.header.get('v', 1) >= 2
-        channel = self._multiplexer.channel(initial_event)
-        hbchan = HeartBeatOnChannel(channel, freq=self._heartbeat_freq,
-                passive=not protocol_v2)
-        bufchan = BufferedChannel(hbchan)
-        event = bufchan.recv()
-        try:
-            functor = self._methods.get(event.name, None)
-            if functor is None:
-                raise NameError(event.name)
-            functor.pattern.process_call(self._context, bufchan, event, functor)
-        except Exception:
-            try:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback,
-                        file=sys.stderr)
-                human_traceback = traceback.format_exc()
-
-                name = exc_type.__name__
-                human_msg = str(exc_value)
-
-                if protocol_v2:
-                    bufchan.emit('ERR', (name, human_msg, human_traceback))
-                else:
-                    bufchan.emit('ERR', (repr(exc_value),))
-            finally:
-                del exc_traceback
-        finally:
-            bufchan.close()
-            bufchan.channel.close()
-            bufchan.channel.channel.close()
-
-    def _acceptor(self):
-        while True:
-            initial_event = self._multiplexer.recv()
-            self._task_pool.spawn(self._async_task, initial_event)
-
-    def run(self):
-        self._acceptor_task = gevent.spawn(self._acceptor)
-        try:
-            self._acceptor_task.get()
-        finally:
-            self._acceptor_task = None
-            self._task_pool.join(raise_error=True)
-
-    def stop(self):
-        if self._acceptor_task is not None:
-            self._acceptor_task.kill(block=False)
+        ServerBase.close(self)
+        SocketBase.close(self)
 
 
-class Client(SocketBase):
+class Client(SocketBase, ClientBase):
     patterns = [PatternReqStream(), PatternReqRep()]
 
     def __init__(self, context=None, timeout=30, heartbeat=5,
             passive_heartbeat=False):
-        super(Client, self).__init__(zmq.XREQ, context=context)
-        self._multiplexer = ChannelMultiplexer(self._events,
-                ignore_broadcast=True)
-        self._timeout = timeout
-        self._heartbeat_freq = heartbeat
-        self._passive_heartbeat = passive_heartbeat
-
-    def __del__(self):
-        self.close()
-        super(Client, self).__del__()
+        SocketBase.__init__(self, zmq.XREQ, context=context)
+        ClientBase.__init__(self, self._events, Client.patterns, timeout,
+                heartbeat, passive_heartbeat)
 
     def close(self):
-        self._multiplexer.close()
-        super(Client, self).close()
-
-    def _raise_remote_error(self, event):
-        self._context.middleware_raise_error(event)
-
-        if event.header.get('v', 1) >= 2:
-            (name, msg, traceback) = event.args
-            raise RemoteError(name, msg, traceback)
-        else:
-            (msg,) = event.args
-            raise RemoteError('RemoteError', msg, None)
-
-    def _select_pattern(self, event):
-        for pattern in Client.patterns:
-            if pattern.accept_answer(event):
-                return pattern
-        msg = 'Unable to find a pattern for: {0}'.format(event)
-        raise RuntimeError(msg)
-
-    def _process_response(self, method, bufchan, timeout):
-        try:
-            try:
-                event = bufchan.recv(timeout)
-            except TimeoutExpired:
-                raise TimeoutExpired(timeout,
-                        'calling remote method {0}'.format(method))
-
-            pattern = self._select_pattern(event)
-            return pattern.process_answer(self._context, bufchan, event, method,
-                timeout, self._raise_remote_error)
-        except:
-            bufchan.close()
-            bufchan.channel.close()
-            bufchan.channel.channel.close()
-            raise
-
-    def __call__(self, method, *args, **kargs):
-        timeout = kargs.get('timeout', self._timeout)
-        channel = self._multiplexer.channel()
-        hbchan = HeartBeatOnChannel(channel, freq=self._heartbeat_freq,
-                passive=self._passive_heartbeat)
-        bufchan = BufferedChannel(hbchan, inqueue_size=kargs.get('slots', 100))
-
-        bufchan.emit(method, args)
-
-        try:
-            if kargs.get('async', False) is False:
-                return self._process_response(method, bufchan, timeout)
-
-            async_result = gevent.event.AsyncResult()
-            gevent.spawn(self._process_response, method, bufchan,
-                    timeout).link(async_result)
-            return async_result
-        except:
-            bufchan.close()
-            bufchan.channel.close()
-            bufchan.channel.channel.close()
-            raise
-
-    def __getattr__(self, method):
-        return lambda *args, **kargs: self(method, *args, **kargs)
+        ClientBase.close(self)
+        SocketBase.close(self)
 
 
 class Pusher(SocketBase):
