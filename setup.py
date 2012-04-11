c14d45a3877dee35bd9486da7678ef157440de62
VanL <van.lindberg@gmail.com>
2012-04-11 09:10:45 -0500

    Update version requirement on pyzmq-static to make it a minimum

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
             'argparse',
             'gevent',
             'msgpack-python',
-            'pyzmq-static==2.1.7',
+            'pyzmq-static>=2.1.7',
     ],
     zip_safe=False,
     scripts=[
