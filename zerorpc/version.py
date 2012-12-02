5c20c57ab9800f3339b329f9a9fe9d0cdc340efc
François-Xavier Bourlet <bombela@gmail.com>
2012-12-01 20:25:12 -0800

    bump version to v0.3.1

--- zerorpc/version.py
+++ zerorpc/version.py
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 __title__ = 'zerorpc'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'dotCloud, Inc.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2012 dotCloud, Inc.'
