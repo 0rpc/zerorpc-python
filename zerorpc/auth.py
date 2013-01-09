aa11cf1761892e449e338f4d829f94540d8eb51a
adieu <adieu@adieu.me>
2013-01-09 16:39:14 +0800

    Modify authentication middleware adapting the latest middleware api change.

--- zerorpc/auth.py
+++ zerorpc/auth.py
@@ -1,49 +1,31 @@
 from hashlib import sha256
 import hmac
 
-from .events import Event
-
 
 class AuthenticationFailedError(Exception):
     pass
 
 
-class SignedEvent(Event):
-    def __init__(self, name, args, context, header=None, create=False):
-        key = header.pop('key', None) if header else None
-        super(SignedEvent, self).__init__(name, args, context, header, create)
-        if key and not 'signature' in self._header:
-            self._header['signature'] = self.build_signature(key)
-        if not key and 'signature' in self._header:
-            self._header['event'] = self
-
-    @property
-    def internal(self):
-        return self._name in ['_zpc_hb', 'ERR', 'OK']
-
-    def string_to_sign(self):
-        string = self._name
-        if self._args:
-            string += '\n%s' % ' '.join(self._args)
-        return string
-
-    def build_signature(self, key):
-        return hmac.new(str(key), self.string_to_sign(), sha256).digest()
+def build_signature(event, key):
+    string = event.name
+    if event.args:
+        string += '\n%s' % ' '.join([str(arg) for arg in event.args])
+    return hmac.new(str(key), string, sha256).digest()
 
 
 class SharedSecretMiddleware(object):
     def __init__(self, key):
         self.key = key
 
-    def load_task_context(self, event_header):
-        if 'signature' in event_header and 'event' in event_header:
-            event = event_header.pop('event')
-            if event_header['signature'] == event.build_signature(self.key):
+    def server_before_exec(self, event):
+        signature = event.header.get('signature', None)
+        if signature and signature == build_signature(event, self.key):
                 return
         raise AuthenticationFailedError
 
-    def get_task_context(self):
-        return {'key': self.key}
+    def client_before_request(self, event):
+        if not 'signature' in event.header:
+            event.header['signature'] = build_signature(event, self.key)
 
 
 class ClientAuthMiddleware(object):
@@ -51,18 +33,20 @@ class ClientAuthMiddleware(object):
         self.client_id = client_id
         self.client_key = client_key
 
-    def get_task_context(self):
-        return {'client_id': self.client_id, 'key': self.client_key}
+    def client_before_request(self, event):
+        if not 'signature' in event.header:
+            event.header['signature'] = build_signature(event, self.client_key)
+        if not 'client_id' in event.header:
+            event.header['client_id'] = self.client_id
 
 
 class BaseServerAuthMiddleware(object):
-    def load_task_context(self, event_header):
-        if all([key in event_header
-               for key in ('signature', 'event', 'client_id')]):
-            client_id = event_header.pop('client_id')
+    def server_before_exec(self, event):
+        signature = event.header.get('signature', None)
+        client_id = event.header.get('client_id', None)
+        if signature and client_id:
             client_key = self.get_client_key(client_id)
-            event = event_header.pop('event')
-            if event_header['signature'] == event.build_signature(client_key):
+            if signature == build_signature(event, client_key):
                 return
         raise AuthenticationFailedError
 
