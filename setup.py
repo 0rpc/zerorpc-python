b02121218b948433924ab624ae2feb2c07db8100
François-Xavier Bourlet <bombela@gmail.com>
2016-03-20 16:29:23 -0700

    Few tags for pypi.

--- setup.py
+++ setup.py
@@ -62,5 +62,8 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ),
 )
