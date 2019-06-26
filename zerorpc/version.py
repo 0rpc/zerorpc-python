30c156dc5166306481fb4f8a1669edcdbb96e244
François-Xavier Bourlet <fx.bourlet@rubrik.com>
2019-06-26 00:44:56 -0700

    Bump version to 0.6.2.

--- zerorpc/version.py
+++ zerorpc/version.py
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 __title__ = 'zerorpc'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __author__ = 'François-Xavier Bourlet <bombela+zerorpc@gmail.com>.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 François-Xavier Bourlet <bombela+zerorpc@gmail.com>.'
