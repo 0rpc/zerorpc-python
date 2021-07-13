1c09a5352ca4e609fa610b341b19901fdd983d24
Shiplu Mokaddim <shiplu.net@gmail.com>
2021-07-14 00:31:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,8 +75,9 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ),
 )
